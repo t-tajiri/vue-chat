17f2614c528b7ae0877d44c2684fc994f65e9279
t-tajiri <swallow.ava@gmail.com>
2020-02-23 20:24:17 +0900

    workaround which occurred types error

--- src/router.ts
+++ src/router.ts
@@ -1,19 +1,23 @@
 import Vue from 'vue'
-import Router, { RouteConfig } from 'vue-router'
+import { VueConstructor } from 'vue/types/umd'
+import Router from 'vue-router'
+import { RouteConfig } from 'vue-router/types/router'
 import Home from '@/views/Home.vue'
+import Chat from '@/views/Chat.vue'
 
 Vue.use(Router)
 
+// workaround define as VueConstructor<Vue>
 const routes: RouteConfig[] = [
   {
     path: '/',
     name: 'home',
-    component: Home
+    component: Home as VueConstructor<Vue>
   },
   {
     path: '/chat',
     name: 'chat',
-    component: () => import(/* webpackChunkName: "about" */ './views/Chat.vue')
+    component: Chat as VueConstructor<Vue>
   }
 ]
 
